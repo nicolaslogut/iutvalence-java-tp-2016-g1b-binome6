bd763987992199d0344ada9e9df0fa9139e7e9ba
Anthony Gelibert <anthony.gelibert@me.com>
2016-05-02 22:51:34 +0200

    Add TODO in Orientation.java

--- Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Orientation.java
+++ Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Orientation.java
@@ -1,30 +1,50 @@
 package fr.iutvalence.logutamilhaud.battleship;
 
+/**
+ * TODO.
+ *
+ * @author TODO
+ * @version TODO
+ */
 public enum Orientation
 {
+	/** TODO. */
 	RIGHT(1,0),
+	/** TODO. */
 	LEFT(-1,0),
+	/** TODO. */
 	TOP(0,-1),
+	/** TODO. */
 	BOTTOM(0,1),
+	/** TODO. */
 	NOTHING(0,0);
-	
+
+	/** TODO. */
 	private final int deltaX;
+	/** TODO. */
 	private final int deltaY;
-	
-	Orientation(int deltaX, int deltaY) 
+
+	/** TODO.
+	 *
+	 * @param deltaX TODO
+	 * @param deltaY TODO
+     */
+	Orientation(int deltaX, int deltaY)
 	{
 		this.deltaX = deltaX;
-		this.deltaY = deltaY; 
+		this.deltaY = deltaY;
 	}
-	
-	public int dX() 
+
+    /** TODO. */
+	public int dX()
 	{
 		return deltaX;
 	}
-	
-	public int dY() 
+
+    /** TODO. */
+	public int dY()
 	{
 		return deltaY;
 	}
-	
+
 }
