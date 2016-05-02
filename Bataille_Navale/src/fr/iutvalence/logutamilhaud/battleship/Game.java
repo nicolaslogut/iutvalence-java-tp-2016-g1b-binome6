6e55816b1dd526eba9463675e5fd228bbb96b2e5
martin amilhaud <martin.amilhaud@gmail.com>
2016-05-02 13:49:52 +0200

    Just a save

--- Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Game.java
+++ Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Game.java
@@ -41,7 +41,7 @@ public class Game
         this.playedPositionPlayer2 = new Board(); 
         this.boatPositionPlayer1 = new Board();
         this.boatPositionPlayer2 = new Board();
-     
+        this.currentPlayer= new Player();
     }
 
     /** Start the game. */
@@ -52,20 +52,28 @@ public class Game
          
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Joueur 1 veuillez saisir vos bateaux");
+      // ---------- Joueur 1 pose ses bateaux ----------
         
+        System.out.println("Joueur 1 veuillez saisir vos bateaux");
+                
         for(int i =0;i<1;i++)
         {
         	System.out.println("Bateau selectionne : "+boats1[i].getNameOfBoat());
 			System.out.println("Veuillez saisir une colonne :");
 			int X = scan.nextInt();
+			while(X<0 || X>9){
+				System.out.println("Veuillez saisir une colonne valide:");
+				X = scan.nextInt();
+			}
 			System.out.println("Veuillez saisir une ligne :");
 			int Y = scan.nextInt();
+			while(Y<0 || Y>9){
+				System.out.println("Veuillez saisir une ligne valide:");
+				Y = scan.nextInt();
+			}
 			scan.nextLine();
 			System.out.println("Veuillez saisir une orientation (RIGHT, LEFT, TOP, BOTTOM) :");
 			String orientation= scan.nextLine();
-			
-
 			
 			try {
 				boatPositionPlayer1.putABoat(boats1[i], X, Y, Orientation.valueOf(orientation));
@@ -80,7 +88,7 @@ public class Game
 			
         }
         
-
+      // ---------- Joueur 2 pose ses bateaux ----------
         
         System.out.println("Joueur 2 veuillez saisir vos bateaux");
         for(int i =0;i<1;i++)
@@ -88,11 +96,20 @@ public class Game
         	System.out.println("Bateau selectionne : "+boats2[i].getNameOfBoat());
 			System.out.println("Veuillez saisir une colonne :");
 			int X = scan.nextInt();
+			while(X<0 || X>9){
+				System.out.println("Veuillez saisir une colonne valide:");
+				X = scan.nextInt();
+			}
 			System.out.println("Veuillez saisir une ligne :");
 			int Y = scan.nextInt();
+			while(Y<0 || Y>9){
+				System.out.println("Veuillez saisir une ligne valide:");
+				Y = scan.nextInt();
+			}
 			scan.nextLine();
 			System.out.println("Veuillez saisir une orientation (RIGHT, LEFT, TOP, BOTTOM) :");
 			String orientation= scan.nextLine();
+			
 			
 			try {
 				boatPositionPlayer2.putABoat(boats2[i], X, Y, Orientation.valueOf(orientation));
@@ -104,19 +121,21 @@ public class Game
 				e1.printStackTrace();
 			}
 			boatPositionPlayer2.displayBoard();
+			
         }
 
         System.out.println(" ");
         System.out.println("La bataille va commencer");
         System.out.println(" ");
 
-        currentPlayer= new Player();
+        
         
 		currentPlayer.setName(player1.getName());
 		
 		while(true)
 		{
-			//Tour joueur 1
+			// ---------- Tour Joueur 1 ----------
+			
 			System.out.println("Coup joue Joueur "+currentPlayer);
 					
 			System.out.println(" ");
@@ -148,7 +167,8 @@ public class Game
 			}
 			
 			
-			//Tour joueur 2
+			// ---------- Tour Joueur 2 ----------
+			
 			System.out.println("Coup joue Joueur "+currentPlayer.getName());
 					
 			System.out.println(" ");
@@ -182,6 +202,6 @@ public class Game
 		}
 		
 		System.out.println("Victoire "+ currentPlayer.getName());
-		
+		scan.close();
     }
 }
