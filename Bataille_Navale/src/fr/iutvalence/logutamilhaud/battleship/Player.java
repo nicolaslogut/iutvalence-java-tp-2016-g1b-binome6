f845f48ae024de15a098d281c8cdf5b812d2b73a
Anthony Gelibert <anthony.gelibert@me.com>
2016-05-02 22:51:55 +0200

    Name is private in Player and there is not "name" setter

--- Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Player.java
+++ Bataille_Navale/src/fr/iutvalence/logutamilhaud/battleship/Player.java
@@ -1,41 +1,30 @@
 package fr.iutvalence.logutamilhaud.battleship;
 
 /**
- * Player for the game
+ * Player for the game.
  *
  * @author logut
  * @version 25/03
  */
 public class Player {
-	/** Name of player. */
-	public String name; 
+    /** Name of player. */
+    private String name;
 
-	/**
-	 * Initialize the name of a player.
-	 * @param name name of a player
+    /**
+     * Initialize the name of a player.
+     *
+     * @param name name of a player
      */
-	public Player(String name) 
-	{
-		this.name = name;
-	}
-	/**
-	 * Get the name of the player.
-	 * @return name of the player
-	 */
-	public String getName() 
-	{
-		return name;
-	}
-	/**
-	 * Set the name of the player
-	 * @param name of the player
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public Player() 
-	{
-		this.name = null;
-	}
+    public Player(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the name of the player.
+     *
+     * @return name of the player
+     */
+    public String getName() {
+        return name;
+    }
 }
